19d3878a9e0d6c1c783d3eea6c9182061fde817e
Trey Pond <trey.pond13@gmail.com>
2022-02-24 22:31:30 -0500

    Chapter 5

--- HIITFit/Views/HistoryView.swift
+++ HIITFit/Views/HistoryView.swift
@@ -33,11 +33,7 @@
 import SwiftUI
 
 struct HistoryView: View {
-  let today = Date()
-  let yesterday = Date().addingTimeInterval(-86400)
-
-  let exercises1 = ["Squat", "Step Up", "Burpee", "Sun Salute"]
-  let exercises2 = ["Squat", "Step Up", "Burpee"]
+  let history = HistoryStore()
 
   var body: some View {
     ZStack(alignment: .topTrailing) {
@@ -48,24 +44,21 @@ struct HistoryView: View {
       .font(.title)
       .padding(.trailing)
       VStack {
-        Text("History")
-          .font(.title)
-          .padding()
+        Text(
+          NSLocalizedString(
+            "History", comment: "view user activity")
+        )
+        .font(.title)
+        .padding()
         Form {
-          Section(
-            header:
-            Text(today.formatted(as: "MMM d"))
-              .font(.headline)) {
-            ForEach(exercises1, id: \.self) { exercise in
-              Text(exercise)
-            }
-          }
-          Section(
-            header:
-            Text(yesterday.formatted(as: "MMM d"))
-              .font(.headline)) {
-            ForEach(exercises2, id: \.self) { exercise in
-              Text(exercise)
+          ForEach(history.exerciseDays) { day in
+            Section(
+              header:
+                Text(day.date.formatted(as: "MMMM d"))
+                .font(.headline)) {
+              ForEach(day.exercises, id: \.self) { exercise in
+                Text(exercise)
+              }
             }
           }
         }
@@ -75,7 +68,7 @@ struct HistoryView: View {
 }
 
 struct HistoryView_Previews: PreviewProvider {
-    static var previews: some View {
-        HistoryView()
-    }
+  static var previews: some View {
+    HistoryView()
+  }
 }
